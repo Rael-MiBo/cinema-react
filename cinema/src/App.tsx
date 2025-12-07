4b62b584e586ea615c246ae16fcf3fda08d6fdff
Rael <121963878+Rael-MiBo@users.noreply.github.com>
2025-12-07 19:10:01 -0300

    input

--- cinema/src/App.tsx
+++ cinema/src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "./components/Button"
+import { Input } from "./components/Input"
 
 function App() {
 
   return (
     <>
+      <Input id="teste" name="nome" placeholder="Digite algo" onChange = {(e) => console.log (e.target.value)} />
       <Button label = "Clique aqui" variant="primary" onClick={() => alert('teste')}/>
     </>
   )
--- cinema/src/components/Input/index.tsx
+++ cinema/src/components/Input/index.tsx
@@ -1,7 +1,26 @@
-export const Input = () => {
+interface InputProps {
+    id?: string;
+    name?: string;
+    type?: 'text' | 'password' | 'email' | 'number' | 'search' | 'tel' | 'url' | 'date' | 'time' | 'month' | 'week' | 'color' | 'file' | 'checkbox' | 'radio' | 'hidden' | 'range' | 'reset' | 'submit' | 'button' | 'image';
+    placeholder?: string;
+    value?: string | number;
+    onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+const idCaptalized = (id: string) => id.charAt(0).toUpperCase() + id.slice(1);
+
+export const Input = ( { id, name, type = 'text',placeholder, value, onChange} : InputProps) => {
     return (
         <>
-            <input type="text" className="input-text" />
+            <label htmlFor={id} className="form-label"> {id} </label>
+            <input
+                id={id}
+                name={name ? name : idCaptalized(id || '')}
+                className="form-control"
+                type={type}
+                placeholder={placeholder}
+                value={value}
+                onChange={onChange}
+            />
         </>
     )
 }
