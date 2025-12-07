313bee790800789511f4bf95c6c631751eaab19f
Rael <121963878+Rael-MiBo@users.noreply.github.com>
2025-12-07 19:31:23 -0300

    routes

--- cinema/src/App.tsx
+++ cinema/src/App.tsx
@@ -1,14 +1,29 @@
-import { Button } from "./components/Button"
-import { Input } from "./components/Input"
+import { BrowserRouter } from "react-router-dom";
+import { Button } from "./components/Button";
+import { Input } from "./components/Input";
+import { Nav } from "./components/Nav";
+import { AppRouters } from "./routes";
 
 function App() {
-
   return (
     <>
-      <Input id="teste" name="nome" placeholder="Digite algo" onChange = {(e) => console.log (e.target.value)} />
-      <Button label = "Clique aqui" variant="primary" onClick={() => alert('teste')}/>
+      <BrowserRouter>
+        <Nav />
+        <AppRouters />
+      </BrowserRouter>
+      <Input
+        id="teste"
+        name="nome"
+        placeholder="Digite algo"
+        onChange={(e) => console.log(e.target.value)}
+      />
+      <Button
+        label="Clique aqui"
+        variant="primary"
+        onClick={() => alert("teste")}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ cinema/src/routes/index.tsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from "react-router-dom"
+import { Home } from "../pages/Home"
+import { Movies } from "../pages/Movies"
+
+
+export const AppRouters = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/movies" element={<Movies />} />
+            </Routes>
+        </>
+    )
+}
